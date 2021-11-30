3826a358b39cbecf6fd61961e5081f3133bfc2d3
l0th3r <alfabetatango9@gmail.com>
2021-11-30 14:39:06 +0100

    added ex01 and ex02

--- main.cpp
+++ main.cpp
@@ -1,9 +1,45 @@
 #include <iostream>
 
-using namespace std;
+// EX01
+std::string string_range(int begin, int end)
+{
+    if(end < begin)
+        return "";
+
+    std::string str = "";
+    int i = begin;
+    while(i <= end)
+    {
+        str += std::to_string(i);
+
+        if(i < end)
+            str += ",";
+
+        i++;
+    }
+
+    return str;
+}
+
+std::string string_range(int end)
+{
+    return string_range(1, end);
+}
+
+// EX02
+double ponderate(double a, double b, double weight)
+{
+    return ((b - a) * weight) + a;
+}
+
+double ponderate(double a, double b)
+{
+    return ponderate(a, b, 0.5);
+}
+
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    std::cout << ponderate(0, 10) << std::endl;
     return 0;
 }
